f20ee73ce0e4517ef66e23b70e939d42dab43e66
Simon Cateau <sorani.dev@outlook.com>
2023-01-21 12:19:04 +0100

    Chapter 5 - Add Images To Your App (TWO METHODS!)

--- src/c2/secondly/lib/main.dart
+++ src/c2/secondly/lib/main.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text("Simon"),
-        ),
-        backgroundColor: Colors.deepPurple[400],
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Center(
+        child: Text("Simon"),
       ),
-      body: const Center(
-        child: Text("Learn to code"),
-      ),
-      backgroundColor: Colors.deepPurple[200],
+      backgroundColor: Colors.deepPurple[400],
     ),
-  ));
+    body: const Center(
+      child:
+          // Image(image: NetworkImage('http://placekitten.com/g/200/300'))),
+
+          Image(image: AssetImage('images/aspen.png')),
+    ),
+    backgroundColor: Colors.deepPurple[200],
+  )));
 }
